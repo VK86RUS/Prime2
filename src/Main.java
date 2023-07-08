b1edc607cdf84bb5f0dd228e4cc7c1272cf05824
BUBA <buba86@ro.ru>
2023-07-08 21:30:21 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 100;
+        int c = 300;
+        int d = 1100;
+        int e = 11;
+        int f = a + c;
+        int g = b + d + e;
+        System.out.println(f);
+        System.out.println(g);
+        
+    }
+}
